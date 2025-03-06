1a965b87a0391ccdad663164802f3b5a41593512
Amine2026 <mohamedamine.lazzez@esprit.tn>
2025-03-06 11:29:42 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,17 +12,22 @@ module.exports = {
         'Background': '#D6D6D6',
         'HeaderColor': '#E6E6E6',
         'HeaderColor2': '#010035',
-        'Available': '#42840F'
+        'Available': '#42840F',
+        'InputBox' : '#D9D9D9',
       },
       fontFamily: {
         'Roboto': ['Roboto', 'sans-serif']
       },
       fontSize: {
-        mainTitle: ["32px", { fontWeight: "bold" }],
-        sectionTitle: ["24px", { fontWeight: "bold" }],
-        subheading: ["20px", { fontWeight: "medium" }],
-        body: ["16px", { fontWeight: "normal" }],
-        small: ["14px", { fontWeight: "light" }],
+        mainTitle: ["32px", { fontWeight: 700 }],
+        footerText: ["32px", { fontWeight: 500 }],
+        headerText2: ["28px", { fontWeight: 500 }],
+        sectionTitle: ["24px", { fontWeight: 700 }],//bold
+        subheading: ["20px", { fontWeight: 500 }],//medium
+        profileText: ["20px", { fontWeight: 400 }],//normal or regular
+        contactusbody: ["16px", { fontWeight: 500 }],
+        body: ["16px", { fontWeight: 400 }],
+        small: ["14px", { fontWeight: 300 }]//light
       },
     },
   },
